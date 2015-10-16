ba024a54aed75cca9310b5329397fd284470bb4b
Stuart Izon <stuart.izon@ovoenergy.com>
2015-10-16 13:22:49 +0100

    Changed build settings/plugins to run locally (without depending on Bruno's nexus)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -1,16 +1,6 @@
 import sbt._
-import Keys._
-import com.bfil.sbt._
-
-object ProjectBuild extends BFilBuild with BFilPlugins {
-
-  lazy val root = BFilProject("future-validation", file("."))
-    .settings(libraryDependencies ++= Dependencies.all)
-    
-}
 
 object Dependencies {
-  
   def all = Seq(
     "org.scalaz" %% "scalaz-core" % "7.1.4",
     "org.specs2" %% "specs2-core" % "3.6.4" % "test",
@@ -18,5 +8,4 @@ object Dependencies {
     "org.mockito" % "mockito-all" % "1.10.8" % "test",
     "org.hamcrest" % "hamcrest-all" % "1.3" % "test"
   )
-  
 }
